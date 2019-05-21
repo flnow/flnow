0450cf808b65e92ccd008c1d2188208613cf55d0
Tuo <guotuo1024@gmail.com>
2019-05-21 17:20:04 +0800

    Fix model format issue

--- models/group_user_map.go
+++ models/group_user_map.go
@@ -2,9 +2,9 @@ package models
 
 import "time"
 
-type GroupUserMap struct{
-	ID    int    `gorm:"Column:id;PRIMARY_KEY;AUTO_INCREMENT" json:"id"`
-	Group int `gorm:"Column:group" json:"group"`
-	User int `gorm:"Column:user" json:"user"`
+type GroupUserMap struct {
+	ID        int       `gorm:"Column:id;PRIMARY_KEY;AUTO_INCREMENT" json:"id"`
+	Group     int       `gorm:"Column:group" json:"group"`
+	User      int       `gorm:"Column:user" json:"user"`
 	CreatedAt time.Time `gorm:"Column:createdAt" json:"createdAt"`
 }
