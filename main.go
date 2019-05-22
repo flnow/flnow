75de16625d4ad2bc2d59927b985f1200ac05fd1b
Tuo <guotuo1024@gmail.com>
2019-05-22 18:02:24 +0800

    ignore go.sum to git

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	// "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 	// "gopkg.in/robfig/cron.v3"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("FL(N)OW")
-	// r := gin.Default()
-	// r.Run(":8080")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8080")
 }
