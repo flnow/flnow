f4c8c495110283b40ce49b429a94480a8e611015
Tuo <guotuo1024@gmail.com>
2019-06-26 10:33:25 +0800

    Fix form tag in structs.

--- modules/flow/flow.go
+++ modules/flow/flow.go
@@ -6,19 +6,19 @@ import (
 
 // Flow entity
 type Flow struct {
-	ID                  int       `gorm:"Column:id;PRIMARY_KEY;AUTO_INCREMENT" json:"id"`
-	Name                string    `gorm:"Column:name;NOT NULL" json:"name" from:"name"`
-	TriggerType         string    `gorm:"Column:triggerType;NOT NULL" json:"triggerType" from:"triggerType"`
-	CronExpression      string    `gorm:"Column:cronExpression;type:varchar(50)" json:"cron"`
-	CronTimeZone        string    `gorm:"Column:cronTimeZone;type:varchar(50)" json:"cronTimeZone"`
-	State               string    `gorm:"Column:state;size:50" json:"state"`
-	Token               string    `gorm:"Column:token;size:255" json:"-"`
-	NodeCount           int       `gorm:"Column:nodeCount" json:"nodeCount"`
-	LastExecutedAt      time.Time `gorm:"Column:lastExecutedAt" json:"lastExecutedAt"`
-	LastExecutedSummary string    `gorm:"Column:lastExecutedSummary;type:text" json:"lastExecutedSummary"`
-	RunAt               string    `gorm:"Column:runAt;type:text" json:"runAt"` // 在哪个worker上被执行
-	HostedOn            string    `gorm:"Column:hostedOn" json:"hostedOn"`     // 在哪里触发，slave
-	Pointer             string    `gorm:"Column:pointer" json:"-"`             // 创建后的cronJob在内存里的ID
+	ID                  int       `gorm:"Column:id;PRIMARY_KEY;AUTO_INCREMENT" json:"id" form:"id"`
+	Name                string    `gorm:"Column:name;NOT NULL" json:"name" form:"name"`
+	TriggerType         string    `gorm:"Column:triggerType;NOT NULL" json:"triggerType" form:"triggerType"`
+	CronExpression      string    `gorm:"Column:cronExpression;type:varchar(50)" json:"cron" form:"cron"`
+	CronTimeZone        string    `gorm:"Column:cronTimeZone;type:varchar(50)" json:"cronTimeZone" form:"cronTimeZone"`
+	State               string    `gorm:"Column:state;size:50" json:"state" form:"state"`
+	Token               string    `gorm:"Column:token;size:255" json:"-" form:"token"`
+	NodeCount           int       `gorm:"Column:nodeCount" json:"nodeCount" form:"nodeCount"`
+	LastExecutedAt      time.Time `gorm:"Column:lastExecutedAt" json:"lastExecutedAt" form:"lastExecutedAt"`
+	LastExecutedSummary string    `gorm:"Column:lastExecutedSummary;type:text" json:"lastExecutedSummary" form:"lastExecutedSummary"`
+	RunAt               string    `gorm:"Column:runAt;type:text" json:"runAt" form:"runAt"` // 在哪个worker上被执行
+	HostedOn            string    `gorm:"Column:hostedOn" json:"hostedOn" form:"hostedOn"`  // 在哪里触发，slave
+	Pointer             string    `gorm:"Column:pointer" json:"-"`                          // 创建后的cronJob在内存里的ID
 
 	Owner int    `gorm:"Column:owner" json:"owner"`
 	Tags  string `gorm:"Column:tags;type:text" json:"tags"`
--- modules/flow/handler.go
+++ modules/flow/handler.go
@@ -17,9 +17,7 @@ func Create(db *gorm.DB) gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, err)
 			return
 		}
-		fmt.Println(flow)
 		flow.initialize()
-		fmt.Println(flow)
 		db.Create(&flow)
 
 		c.JSON(http.StatusOK, flow)
@@ -29,12 +27,7 @@ func Create(db *gorm.DB) gin.HandlerFunc {
 // Detail of Flow
 func Detail(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 		fmt.Println("Flow Detail...")
-		// fmt.Println(c.Param("test_empty"))
-		// if c.Param("flowID") == "" {
-		// 	c.String(http.StatusInternalServerError, "No ID")
-		// }
 		flow := new(Flow)
 		db.Where("id = ?", c.Param("flowID")).First(flow)
 		if flow.ID == 0 {
@@ -50,13 +43,10 @@ func Update(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		fmt.Println("Flow Update...")
 		flow := new(Flow)
-		fmt.Println(flow)
 		if err := c.Bind(flow); err != nil {
 			c.JSON(http.StatusInternalServerError, err)
 			return
 		}
-
-		fmt.Println(flow)
 		db.Model(&flow).Where("id = ?", flow.ID).Updates(flow)
 
 		c.JSON(http.StatusOK, flow)
