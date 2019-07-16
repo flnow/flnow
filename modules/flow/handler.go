4ad8729a4eb439e56913e78070c4e3061546d7a6
Tuo <guotuo1024@gmail.com>
2019-07-16 15:34:29 +0800

    Remove debug logs

--- modules/flow/handler.go
+++ modules/flow/handler.go
@@ -21,12 +21,9 @@ func Create(db *gorm.DB) gin.HandlerFunc {
 		flow.initialize()
 		if !db.HasTable(&Flow{}) {
 			db.CreateTable(&Flow{})
+			db.CreateTable(&Node{})
+			db.CreateTable(&NodeConfiguration{})
 		}
-		// var pl Pipeline
-		// json.Unmarshal([]byte(flow.Pipeline), &pl)
-
-		// var pipeline Pipeline
-		// pipeline.Unmarshal(flow.Pipeline)
 
 		pipe := Pipeline{}
 		if flow.Pipeline != "{}" && len(flow.Pipeline) > 0 {
@@ -34,7 +31,7 @@ func Create(db *gorm.DB) gin.HandlerFunc {
 				fmt.Println(err)
 			}
 		}
-		transaction := db.Begin()
+		tranx := db.Begin()
 
 		if !pipe.IsZero() {
 			// not zero value pipeline
@@ -42,15 +39,24 @@ func Create(db *gorm.DB) gin.HandlerFunc {
 			// Add more steps to transaction
 			nodes, nodeConfigs := pipe.ToRelational(flow.ID, "", "", 1)
 
+			for _, n := range nodes {
+				if err := tranx.Create(&n).Error; err != nil {
+					tranx.Rollback()
+				}
+			}
+			for _, c := range nodeConfigs {
+				tranx.Create(&c)
+			}
+
 			bNodes, _ := json.Marshal(nodes)
 			bConfigs, _ := json.Marshal(nodeConfigs)
 			fmt.Println(string(bNodes))
 			fmt.Println(string(bConfigs))
 		}
-		transaction.LogMode(true)
-		transaction.Create(&flow)
-		transaction.LogMode(false)
-		transaction.Commit()
+
+		tranx.Create(&flow)
+
+		tranx.Commit()
 		c.JSON(http.StatusOK, flow)
 	}
 }
